29e7ee3a8fa4b42e73930f2eb3a0aaf54c438af8
Rasel-ahemd <nhd305812@gmail.com>
2022-02-17 03:01:48 +0500

    add validation

--- js/app.js
+++ js/app.js
@@ -12,32 +12,54 @@ let saveammount = document.getElementById("saveammount");
 let remainingAmmount = document.getElementById("remainingBalance");
 // all addevent listeners
 calculateBtn.addEventListener("click", function () {
+  // checking validation
+  if (
+    income.value === "" ||
+    income.value <= 0 ||
+    food.value === "" ||
+    food.value <= 0 ||
+    rent.value === "" ||
+    rent.value <= 0 ||
+    clothes.value === "" ||
+    clothes.value <= 0
+  ) {
+    alert("Please provide the valid number!");
+    return;
+  }
+  if (expenses() > incomes()) {
+    alert("Yoru expenses more than incomes");
+    return;
+  }
   totalExpense.innerText = expenses();
   balance.innerText = incomes() - expenses();
 });
+// save button click
 saveBtn.addEventListener("click", function () {
-  saveammount.innerText = (incomes() * saves()) / 100;
+  if (save.value < 0 || save.value == "") {
+    alert("Please provide the valid number");
+    return;
+  }
+  let totalSaveAmmount = (incomes() * saves()) / 100;
+  saveammount.innerText = totalSaveAmmount;
   remainingAmmount.innerText =
     parseInt(balance.textContent) - parseInt(saveammount.innerText);
 });
+// total income calculator
 function incomes() {
   let incomeValue = parseInt(income.value);
   return incomeValue;
 }
+// total expense calculator
 function expenses() {
   let foodValue = parseInt(food.value);
   let rentValue = parseInt(rent.value);
-  let closeValue = parseInt(clothes.value);
-  return foodValue + rentValue + closeValue;
+  let clotheValue = parseInt(clothes.value);
+  return foodValue + rentValue + clotheValue;
 }
+// total saves calculator
 function saves() {
-  if (save.value <= 0 || save.value === "") {
-    alert("j");
-    return save.value;
-  } else {
-    let saveValue = parseInt(save.value);
-    return saveValue;
-  }
+  let saveValue = parseInt(save.value);
+  return saveValue;
 }
 
 // let calculateBtn = document.getElementById("totalCalBtn");
